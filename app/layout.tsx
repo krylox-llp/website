e4f8b6edbdece4e1caa1e85b2bcfa60442f98efa
Jagadeesh J <jagadeesh@jsconsulting.in>
2026-03-08 01:07:57 +0530

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -220,6 +221,7 @@ export default function RootLayout({
         />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-Z4DCZ8MF4R" />
     </html>
   );
 }
